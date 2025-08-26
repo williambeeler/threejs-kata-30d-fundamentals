b1ddb36eed2cb45c71fe8ad8178e010280ff5621
William Beeler <will@williambeeler.com>
2025-08-26 10:11:05 -0500

    Added lil-gui to day 1

--- 1_hello_cube/src/main.js
+++ 1_hello_cube/src/main.js
@@ -1,5 +1,13 @@
-import * as THREE from "three";
+import * as THREE from "three"
+import { GUI } from 'lil-gui'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+//Gui
+const gui = new GUI()
+const lightFolder = gui.addFolder('Light Folder')
+const geometryFolder = gui.addFolder('Cube')
+const animationFolder = gui.addFolder('Animation')
+
 
 // Scene
 const scene = new THREE.Scene();
@@ -30,6 +38,8 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // Light
 const ambientLight = new THREE.AmbientLight(0xD6D6D6, 0.9);
 scene.add(ambientLight);
+lightFolder.add(ambientLight, 'intensity', 0, 10, 0.1).name('Ambient Light Intensity')
+lightFolder.addColor(ambientLight, 'color').name('Ambient Light Color')
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 10, 7);
@@ -41,6 +51,18 @@ const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+// Lil-gui mesh manipulation
+const geometryParams = { width: 1, height: 1, depth: 1 };
+function rebuildBox() {
+  cube.geometry.dispose();
+  cube.geometry = new THREE.BoxGeometry(geometryParams.width, geometryParams.height, geometryParams.depth);
+}
+geometryFolder.add(geometryParams, 'width', 0.1, 10, 0.1).onChange(rebuildBox);
+geometryFolder.add(geometryParams, 'height', 0.1, 10, 0.1).onChange(rebuildBox);
+geometryFolder.add(geometryParams, 'depth', 0.1, 10, 0.1).onChange(rebuildBox);
+
+
+
 // Handle Resize
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -49,10 +71,12 @@ window.addEventListener("resize", () => {
 });
 
 // Animation Loop
+let rotationSpeed = { speed: 0.01 }
+animationFolder.add(rotationSpeed, 'speed', 0, 5, 0.01).name('Rotation Speed')
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += rotationSpeed.speed;
+  cube.rotation.y += rotationSpeed.speed;
   controls.update();
   renderer.render(scene, camera);
 }
