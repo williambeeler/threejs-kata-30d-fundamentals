5392fbd96d807ac8c37d753107dc15b9cdd8e686
William Beeler <will@williambeeler.com>
2025-09-04 10:14:19 -0500

    Three.js Journey added to a folder

---
+++ three-js-journey/01-first/script.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+const canvas = document.querySelector('canvas.webgl');
+
+console.log(THREE);
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1,1,1);
+
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3;
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera)
